c83389c09e4dccd2cddba025f3499a414d1a1569
criscaetano3 <criscaetano12k@gmail.com>
2026-04-14 11:37:08 -0300

    commit_5

--- script.js
+++ script.js
@@ -41,3 +41,47 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+// botão saiba mais
+const botoes = document.querySelectorAll(".btn-projeto");
+
+botoes.forEach((botao) => {
+  botao.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const cardAtual = this.closest(".card");
+    const todosCards = document.querySelectorAll(".card");
+
+    const jaEstaAtivo = cardAtual.classList.contains("ativo");
+
+    // fecha todos
+    todosCards.forEach((card) => {
+      card.classList.remove("ativo");
+      card.querySelector(".btn-projeto").textContent = "Saiba mais";
+    });
+
+    // se NÃO estava aberto → abre
+    if (!jaEstaAtivo) {
+      cardAtual.classList.add("ativo");
+      this.textContent = "Ver menos";
+    }
+  });
+});
+
+// Botão ver mais projetos
+const botao = document.querySelector(".btn-ver-mais");
+const ocultos = document.querySelectorAll(".oculto");
+
+let aberto = false;
+
+botao.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  ocultos.forEach((card) => {
+    card.classList.toggle("mostrar");
+  });
+
+  aberto = !aberto;
+
+  botao.textContent = aberto ? "ver menos" : "Ver todos projetos";
+});
